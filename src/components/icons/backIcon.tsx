8ec1f4e8316b78f0202a9a9d757d24167da5b3c4
Mariia Zaitseva <zaitcevamasha39@gmail.com>
2022-10-15 14:03:11 +0100

    mock data for book chapters, header, menu

---
+++ src/components/icons/backIcon.tsx
@@ -0,0 +1,13 @@
+export const BackIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="10.823" height="16.857" viewBox="0 0 10.823 16.857">
+            <g id="Icon_back" data-name="Icon, back" transform="translate(1.412 1.411)">
+                <g id="icn_back" transform="translate(8 14.035) rotate(180)">
+                    <line id="Line_17" data-name="Line 17" y1="6.981" x2="8" transform="translate(0 7.053)" fill="none" stroke="#000" stroke-linecap="square" stroke-width="2"/>
+                    <path id="Path_44" data-name="Path 44" d="M0,0,7.921,7.035" transform="translate(0.079)" stroke="#000" stroke-linecap="square" stroke-width="2"/>
+                </g>
+            </g>
+        </svg>
+
+    )
+}
