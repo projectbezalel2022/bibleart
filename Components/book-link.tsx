a54298d39f212164c1d13cd61f584f39fc607548
samsonhumber <printerfissh693@gmail.com>
2022-10-15 10:50:48 +0100

    contents page works, currently not stlyed

--- Components/book-link.tsx
+++ Components/book-link.tsx
@@ -9,32 +9,44 @@ type propsType = {
 
 export default function BookLink({book, chapters}: propsType) {
     const [bookIsSelected, setBookIsSelected] = useState<'hidden'|'visible'>('hidden');
+    const [chaptersShown, setChaptersShown] = useState<Number[]>([]);
     //let bookIsSelected: 'hidden'|'visible' = 'hidden';
-    const chaptersArray = Array(chapters);
+    let fullChaptersArray: Number[] = [];
+    for (let i=0; i<chapters; i++) {
+        fullChaptersArray.push(i+1);
+    }
+    //console.log(chaptersArray)
     function onBookClick (e: ChangeEvent<HTMLElement>) {
         // What is revealed shall be hidden, and what is hidden shall now be revealed!
         if (bookIsSelected === 'visible') {
             setBookIsSelected('hidden');
+            setChaptersShown([]);
+            console.log(chaptersShown);
         } else {
             console.log('hidden');
             setBookIsSelected('visible');
+            setChaptersShown(fullChaptersArray);
         }
         
         console.log('Book selected');
     }
     return (<li>
         <Link href='' onClick={onBookClick}>{book}</Link>
-        <ul style={{visibility: bookIsSelected}}><li>Hi mate</li></ul>
+        <ul style={{visibility: bookIsSelected}}>
+            {chaptersShown.map((chapter, index) => {
+                let linkRef = '/books/'+book+'/'+String(chapter);
+                //console.log(linkRef);
+                return (<li key={index}>
+                    <Link href={linkRef}>{String(chapter)}</Link>
+                </li>)
+            })}
+        </ul>
       </li> 
       )
 }
 
 /*
 <ol style={{visibility: bookIsSelected}}>
-            {chaptersArray.map((chapter, index) => {
-                return (<li key={index}>
-                    <Link href={'/books/'+book+'/'+String(chapter)}>{String(chapter)}</Link>
-                </li>)
-            })}
+            
         </ol>
 */
