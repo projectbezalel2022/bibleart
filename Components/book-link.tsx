4c2758c2960e9989e5321d354259bc90f4845c19
samsonhumber <printerfissh693@gmail.com>
2022-10-15 09:06:18 +0100

    started using an experimental link feature, so code might be compromised if this goes wrong

--- Components/book-link.tsx
+++ Components/book-link.tsx
@@ -7,22 +7,34 @@ type propsType = {
     chapters: Number
 }
 
-export default function BookLink({key, book, chapters}: propsType) {
-    const [bookIsSelected, setBookIsSelected] = useState(false);
+export default function BookLink({book, chapters}: propsType) {
+    const [bookIsSelected, setBookIsSelected] = useState<'hidden'|'visible'>('hidden');
+    //let bookIsSelected: 'hidden'|'visible' = 'hidden';
     const chaptersArray = Array(chapters);
-    function onBookClick () {
-        setBookIsSelected(!bookIsSelected);
+    function onBookClick (e: ChangeEvent<HTMLElement>) {
+        // What is revealed shall be hidden, and what is hidden shall now be revealed!
+        if (bookIsSelected === 'visible') {
+            setBookIsSelected('hidden');
+        } else {
+            console.log('hidden');
+            setBookIsSelected('visible');
+        }
+        
         console.log('Book selected');
     }
     return (<li>
         <Link href='' onClick={onBookClick}>{book}</Link>
-        <ol>
-            {chaptersArray.map((chapter, index) => {
-                return (<li key={index}>
-                    <Link href={'/book-chapter/'+book+'/'+String(chapter)}>{String(chapter)}</Link>
-                </li>)
-            })}
-        </ol>
+        <ul style={{visibility: bookIsSelected}}><li>Hi mate</li></ul>
       </li> 
       )
 }
+
+/*
+<ol style={{visibility: bookIsSelected}}>
+            {chaptersArray.map((chapter, index) => {
+                return (<li key={index}>
+                    <Link href={'/books/'+book+'/'+String(chapter)}>{String(chapter)}</Link>
+                </li>)
+            })}
+        </ol>
+*/
--- pages/books/[...chapter].tsx
+++ pages/books/[...chapter].tsx
@@ -3,5 +3,5 @@ import { useRouter } from "next/router";
 export default function BookChapter() {
     const router = useRouter();
     const { book, chapter } = router.query;
-    return <p>Book: {book}</p>
+    return <p>Chapter: {[book, chapter]}</p>
 }
